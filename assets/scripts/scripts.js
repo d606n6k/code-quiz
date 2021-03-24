bbecefb5a7ac7a8867a05c5c6d6b0f3b645f2af5
William Lucht <aaronlucht@gmail.com>
2021-03-23 19:00:49 -0700

    cleaned up js for project submission

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,44 +1,3 @@
-// USER STORY
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// - I will need a start button in my html - DONE
-//  - will I need to store all cards in an Object?
-// THEN a timer starts and I am presented with a question
-// - I will need to start the timer AND swap data-cards
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-
-// FIRST PASS/ITERATION PSEUDO-CODE
-// Pseudo-Code
-// start page html
-// - start div with the following elements
-// -- title
-// -- start quiz btn
-// -- directions
-// - time: 0
-// - view high scores link
-// add a little css don't make it perfect yet
-// quiz js
-// create variables for referencing els
-// - start div
-// - high scores link
-// - time
-// - start btn
-// vars for values
-// - timeRemaining
-// - highScores
-// - questions: Array of questions
-// Question Object
-// - question: String
-// - options: String[]
-// - answer: String (one of the options)
-
 // Global Variable Handles
 var cardContainer = document.getElementById("#code-card-swapper");
 var cardStart = document.getElementById("card-start");
@@ -66,11 +25,6 @@ function quizStart(){
     // remove the starter card
     cardStart.classList.add("hidden");
     document.getElementById("q1").classList.remove("hidden");
-    // for (let i = 0; i < 5; i++) {
-    //   document.getElementById(`q${i}`).classList.remove("hidden");
-    //   document.getElementById(`q${i-1}`).classList.add("hidden");
-    // }
-    
 };
 
 // Clicked Answer Button
